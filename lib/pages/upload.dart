50105c5510d5c2ff61337638d7d82e0d685eca0f
Ashwini Kumar Priyanshu <priyanshuashwinikumar@gmail.com>
2020-10-19 18:24:55 +0530

    Upload Page Added

--- lib/pages/upload.dart
+++ lib/pages/upload.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Upload extends StatefulWidget {
   @override
@@ -6,8 +7,42 @@ class Upload extends StatefulWidget {
 }
 
 class _UploadState extends State<Upload> {
+  selectImage() {}
+
+  Container buildSplashScreen() {
+    return Container(
+      color: Theme.of(context).accentColor.withOpacity(0.6),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          SvgPicture.asset(
+            'assets/images/upload.svg',
+            height: 260.0,
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 20.0),
+            child: RaisedButton(
+              onPressed: () => selectImage(context),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              child: Text(
+                'Upload Image',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 22.0,
+                ),
+              ),
+              color: Colors.deepOrange,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text("Upload");
+    return buildSplashScreen();
   }
 }
