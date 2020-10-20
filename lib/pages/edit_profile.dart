ccb18a68dbe3a5f48e239e7a44651e8a8a7a1f1c
Ashwini Kumar Priyanshu <priyanshuashwinikumar@gmail.com>
2020-10-21 00:33:49 +0530

    Edit profile page completed

--- lib/pages/edit_profile.dart
+++ lib/pages/edit_profile.dart
@@ -1,4 +1,9 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import "package:flutter/material.dart";
+import 'package:fluttershare/models/user.dart';
+import 'package:fluttershare/pages/home.dart';
+import 'package:fluttershare/widgets/progress.dart';
 
 class EditProfile extends StatefulWidget {
   final String currentUserId;
@@ -9,7 +14,10 @@ class EditProfile extends StatefulWidget {
 }
 
 class _EditProfileState extends State<EditProfile> {
+  TextEditingController displayNameController = TextEditingController();
+  TextEditingController bioController = TextEditingController();
   bool isLoading = false;
+  User user;
 
   @override
   void initState() {
@@ -17,10 +25,145 @@ class _EditProfileState extends State<EditProfile> {
     getUser();
   }
 
-  getUser() async {}
+  getUser() async {
+    setState(() {
+      isLoading = true;
+    });
+    DocumentSnapshot doc = await usersRef.document(widget.currentUserId).get();
+    //deserializing our document
+    user = User.fromDocument(doc);
+    displayNameController.text = user.displayName;
+    bioController.text = user.bio;
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  Column buildDisplayNameField() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(top: 12.0),
+          child: Text(
+            'Display Name',
+            style: TextStyle(color: Colors.grey),
+          ),
+        ),
+        TextField(
+          controller: displayNameController,
+          decoration: InputDecoration(
+            hintText: 'Update Display Name',
+          ),
+        ),
+      ],
+    );
+  }
+
+  Column buildBioField() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(top: 12.0),
+          child: Text(
+            'Bio',
+            style: TextStyle(color: Colors.grey),
+          ),
+        ),
+        TextField(
+          controller: bioController,
+          decoration: InputDecoration(
+            hintText: 'Update Bio',
+          ),
+        ),
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Text('Edit Profile');
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text(
+          'Edit Profile',
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.done,
+              size: 30.0,
+              color: Colors.green,
+            ),
+            onPressed: () => Navigator.pop(context),
+          )
+        ],
+      ),
+      body: isLoading
+          ? circularProgress()
+          : ListView(
+              children: <Widget>[
+                Container(
+                  child: Column(
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.only(
+                          top: 16.0,
+                          bottom: 8.0,
+                        ),
+                        child: CircleAvatar(
+                          radius: 50.0,
+                          backgroundImage: CachedNetworkImageProvider(
+                            user.photoUrl,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(16.0),
+                        child: Column(
+                          children: <Widget>[
+                            buildDisplayNameField(),
+                            buildBioField(),
+                          ],
+                        ),
+                      ),
+                      RaisedButton(
+                        onPressed: () => Navigator.pop(context),
+                        child: Text(
+                          'Update Profile',
+                          style: TextStyle(
+                            color: Theme.of(context).primaryColor,
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(16.0),
+                        child: FlatButton.icon(
+                          onPressed: () => print('logout'),
+                          icon: Icon(
+                            Icons.cancel,
+                            color: Colors.red,
+                          ),
+                          label: Text(
+                            'Logout',
+                            style: TextStyle(
+                              color: Colors.red,
+                              fontSize: 20.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+    );
   }
 }
