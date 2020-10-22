f44251bdef9690f54f27c6d62819dcfbb76d5f7d
Ashwini Kumar Priyanshu <priyanshuashwinikumar@gmail.com>
2020-10-22 21:09:52 +0530

    Comments page added

--- lib/pages/comments.dart
+++ lib/pages/comments.dart
@@ -1,14 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:fluttershare/widgets/header.dart';
 
 class Comments extends StatefulWidget {
+  final String postId;
+  final String postOwnerId;
+  final String postMediaUrl;
+
+  const Comments({
+    Key key,
+    this.postId,
+    this.postOwnerId,
+    this.postMediaUrl,
+  }) : super(key: key);
+
   @override
-  CommentsState createState() => CommentsState();
+  CommentsState createState() => CommentsState(
+      postId: this.postId,
+      postOwnerId: this.postOwnerId,
+      postMediaUrl: this.postMediaUrl);
 }
 
 class CommentsState extends State<Comments> {
+  TextEditingController commentController = TextEditingController();
+  final String postId;
+  final String postOwnerId;
+  final String postMediaUrl;
+
+  CommentsState({
+    Key key,
+    this.postId,
+    this.postOwnerId,
+    this.postMediaUrl,
+  });
+
+  buildComments() {
+    return Text('ff');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('Comments');
+    return Scaffold(
+      appBar: header(context, titleText: 'Comments'),
+      body: Column(
+        children: <Widget>[
+          Expanded(child: buildComments()),
+          Divider(),
+          ListTile(
+            title: TextFormField(
+              controller: commentController,
+              decoration: InputDecoration(
+                labelText: 'Write a comment...',
+              ),
+            ),
+            trailing: OutlineButton(
+              onPressed: () {},
+              borderSide: BorderSide.none,
+              child: Text('Post'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
 
