9723b354c3f2415575d71446b8dfd06ad1c654ae
Ashwini Kumar Priyanshu <priyanshuashwinikumar@gmail.com>
2020-10-22 19:00:43 +0530

    Comments Page added

--- lib/widgets/post.dart
+++ lib/widgets/post.dart
@@ -4,6 +4,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttershare/models/user.dart';
+import 'package:fluttershare/pages/comments.dart';
 import 'package:fluttershare/pages/home.dart';
 import 'package:fluttershare/widgets/custom_image.dart';
 import 'package:fluttershare/widgets/progress.dart';
@@ -181,7 +182,7 @@ class _PostState extends State<Post> {
                     scale: animatorState.value,
                     child: Icon(
                       Icons.favorite,
-                      size: 160.0,
+                      size: 100.0,
                       color: Colors.red,
                     ),
                   ),
@@ -213,7 +214,12 @@ class _PostState extends State<Post> {
               padding: EdgeInsets.only(right: 20.0),
             ),
             GestureDetector(
-              onTap: () => print('liking post'),
+              onTap: () => showComments(
+                context,
+                postId: postId,
+                ownerId: ownerId,
+                mediaUrl: mediaUrl,
+              ),
               child: Icon(
                 Icons.chat,
                 size: 28.0,
@@ -270,4 +276,15 @@ class _PostState extends State<Post> {
       ],
     );
   }
+}
+
+showComments(BuildContext context,
+    {String postId, String ownerId, String mediaUrl}) {
+  Navigator.push(context, MaterialPageRoute(builder: (context) {
+    return Comments(
+      postId: postId,
+      postOwnerId: ownerId,
+      postMediaUrl: mediaUrl,
+    );
+  }));
 }
